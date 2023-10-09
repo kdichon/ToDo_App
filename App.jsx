46b00f0b267aa2b4d421bd924dd632d4a21a6331
Kdichon <herve@kdichon.fr>
2023-10-09 13:52:54 -0400

    components

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
+import Todo from './src/Todo';
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      {/* <Text>App</Text> */}
+      <Todo />
     </View>
   );
 };
